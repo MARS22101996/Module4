412c18643c929aaa1274cd4dd6d79c8855893081
Mariia Suvalova <mariia_suvalova@epam.com>
2017-08-21 00:43:55 +0300

    Parallel foreach

--- HashCalculator/HashCalculator/ViewModels/FilesCalculatorViewModel.cs
+++ HashCalculator/HashCalculator/ViewModels/FilesCalculatorViewModel.cs
@@ -6,6 +6,10 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Xml.Serialization;
 
 namespace HashCalculator.Calculator.Concrete
@@ -14,6 +18,7 @@ namespace HashCalculator.Calculator.Concrete
     {       
         private string[] _filePaths;
         private List<FileInformation> _files;
+        private static object thLockMe = new object();
 
         private List<FileInformation> _filesInfo;
         public List<FileInformation> FilesInfo
@@ -33,38 +38,55 @@ namespace HashCalculator.Calculator.Concrete
         {           
             _files = new List<FileInformation>();
         }
-        public List<FileInformation> ConfigureFileInfo(string path)
+        public void ConfigureFileInfo(string path)
         {
             _filePaths = Directory.GetFiles(path, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
 
             using (var md5 = MD5.Create())
             {
-                foreach (var filePath in _filePaths)
+                //foreach (var filePath in _filePaths)
+                //{
+                //    var file = CollectInformation(filePath, md5);
+
+                //    RecordResultsInAnXMLFile(file);
+
+                //    InputOfResultsIntoTheControl(file);
+                //}          
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                var paralelOptions = new ParallelOptions
                 {
-                    var file = CollectInformation(filePath, md5);
+                    MaxDegreeOfParallelism = 3,
+                    CancellationToken = cts.Token
+                };
+                Parallel.ForEach(_filePaths, paralelOptions,
+                    filePath => 
+                        {
+                            var file = CollectInformation(filePath, md5);
 
-                    RecordResultsInAnXMLFile(file);
+                            RecordResultsInAnXMLFile(file);
 
-                    InputOfResultsIntoTheControl(file);
-                }
-
-                return _files;
+                            InputOfResultsIntoTheControl(file);
+                        }
+                        );
             }
         }
 
         private FileInformation CollectInformation(string filePath, MD5 md5)
         {
             var info = new FileInformation();
-
-            using (var stream = File.OpenRead(filePath))
+            lock (thLockMe)
             {
-                info.Path = filePath;
+                using (var stream = File.OpenRead(filePath))
+                {
+                    info.Path = filePath;
 
-                info.Hash = Encoding.Default.GetString(md5.ComputeHash(stream));
+                    info.Hash = Encoding.Default.GetString(md5.ComputeHash(stream));
 
-                info.Length = stream.Length;              
+                    info.Length = stream.Length;
+                }
             }
-
+           
             return info;
         }
 
@@ -92,21 +114,25 @@ namespace HashCalculator.Calculator.Concrete
         public static void SerializeAppend<FileInformation>(FileInformation obj, string path)
         {
             var writer = new XmlSerializer(typeof(FileInformation));
+            lock (thLockMe)
+            {
+                FileStream file = File.Open(path, FileMode.Append, FileAccess.Write);
 
-            FileStream file = File.Open(path, FileMode.Append, FileAccess.Write);
+                writer.Serialize(file, obj);
 
-            writer.Serialize(file, obj);
-
-            file.Close();
+                file.Close();
+            }
         }
 
         public static void Serialize<FileInformation>(FileInformation obj, string path)
         {
             var writer = new XmlSerializer(typeof(FileInformation));
-
-            using (var file = new StreamWriter(path))
+            lock (thLockMe)
             {
-                writer.Serialize(file, obj);
+                using (var file = new StreamWriter(path))
+                {
+                    writer.Serialize(file, obj);
+                }
             }
         }
 
